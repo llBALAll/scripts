e5318f9c64a3c7aa4b7cde4a753435f36487b52c
llBALAll <43710708+llBALAll@users.noreply.github.com>
2022-03-30 20:57:08 -0300

    Update findpatterninfiles.sh

--- shell/findpatterninfiles.sh
+++ shell/findpatterninfiles.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Busca por patterns em arquivos, procurando por padroes de tipos de arquivos conhecidos que nao estejam criptografados.
+scriptname=`echo $0 | sed 's/\//\/\n/g' | tail -n 1`
 
-
-if [ "$1" = "" ] || [ "$2" != "" ] ; then
+if [ -z "$1" ] || [ -n "$2" ]; then
         echo
-        echo "  Usage: $0 <FileToSearchPattern>"
+        echo "Script to search for pattern bytes in a file to match file type by default headers;"
+        echo "  Usage: $scriptname <file>"
         echo
         exit 1
-else
-        hd $1 | grep "00000000  7f 45 4c 46 02 01 01 00  00 00 00 00 00 00 00 00  |.ELF............|" 1>/dev/null
-        if [ $? -eq 0 ] ; then
-                echo "ELF file"
-        fi
-
-        hd $1 | grep "00000000  4d 5a 90 00 03 00 00 00  04 00 00 00 ff ff 00 00  |MZ..............|" 1>/dev/null
-        if [ $? -eq 0 ] ; then
-                echo "PE file"
-        fi
-
-        exit 0
 fi
+
+# hexdump -C $1 | grep "00000000  7f 45 4c 46 02 01 01 00  00 00 00 00 00 00 00 00  |.ELF............|" 1>/dev/null
+xxd -p -c 16 -l 16 $1 | grep "7f454c46020101000000000000000000" 1>/dev/null
+if [ $? -eq 0 ] ; then echo "ELF file"; fi
+
+# hexdump -C $1 | grep "00000000  4d 5a 90 00 03 00 00 00  04 00 00 00 ff ff 00 00  |MZ..............|" 1>/dev/null
+xxd -p -c 16 -l 16 $1 | grep "4d5a90000300000004000000ffff0000" 1>/dev/null
+if [ $? -eq 0 ] ; then echo "PE file"; fi
+
+exit 0
