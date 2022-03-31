bd62f34283ea3d471d33af56db5039d66674e571
llBALAll <43710708+llBALAll@users.noreply.github.com>
2022-03-30 21:13:43 -0300

    Update ipforward_nat.sh

--- shell/ipforward_nat.sh
+++ shell/ipforward_nat.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# Esse script habilita o roteamento entre subredes (IP_FORWARD) e o NAT na interface WAN(internet)
+scriptname=`echo $0 | sed 's/\//\/\n/g' | tail -n 1`
 
-
-if [ "$1" = "" ] || [ "$2" != "" ]; then
-	echo "Passe a interface WAN como argumento 1"
-	echo "  ex: ./ipforward_nat.sh eth0"
+if [ -z "$1" ] || [ -n "$2" ]; then
+	echo
+	echo " Script to enable (IP_FORWARD) and NAT on a specific network interface"
+	echo "  Usage: $scriptname <interface>"
 	exit 1
-else
-	net_ifaces=`ip addr show | grep -i state | tr -d " " | cut -d ":" -f 2`	# variavel que contem todas as interfaces de rede da maquina
+fi
 
-	flag_argumento="0"
-	for i in $net_ifaces; do	# loop p/ varrer a variavel que contem todas as interfaces de rede da maquina
-		if [ "$1" = "$i" ]; then	# testa de o argumento passado pelo usuario corresponde a alguma interface de rede da maquina
-			flag_argumento="1"	# flag_argumento para detectar se o argumento passado pelo usuario corresponde a alguma interface de rede valida
-		fi
-	done
-	
-	if [ "$flag_argumento" = "1" ]; then	# caso o usuario tenha passado um a interface valida como argumento para o script
-		echo "1" > /proc/sys/net/ipv4/ip_forward	# habilita o roteamento de pacotes entre subredes
-		iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE	# habilita o NAT na interface WAN passada como argumento
-		exit 0
-	else
-		echo "Interface WAN na encontrada!"
-		echo "(P/ listar as interfaces de rede use: ip addr show)"
-		exit 1
+net_ifaces=`ip addr show | grep -i state | tr -d " " | cut -d ":" -f 2`
+
+flag_arg="0"
+for i in $net_ifaces; do
+	if [ "$1" = "$i" ]; then
+		flag_arg="1"
 	fi
+done
+
+if [ "$flag_arg" = "1" ]; then
+	echo "1" > /proc/sys/net/ipv4/ip_forward
+	iptables -t nat -A POSTROUTING -o $1 -j MASQUERADE
+	exit 0
+else
+	echo "Interface not found!"
+	exit 1
 fi
