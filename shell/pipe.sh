8fb38f8a109d9097dfc198e6e4a8ad51402f152f
llBALAll <43710708+llBALAll@users.noreply.github.com>
2022-04-06 23:03:38 -0300

    Update pipe.sh

--- shell/pipe.sh
+++ shell/pipe.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-scriptname=`echo $0 | sed 's/\//\/\n/g' | tail -n 1`
-
 if [ ! -p /dev/stdin ] || [ -z "$1" ]; then
         echo
         echo " Script to redirect a command output to another command that dont accept pipe input"
-        echo "  Usage: command1 | ./$scriptname command2"
+        echo "  Usage: command1 | $0 command2"
         echo "  ex: cat hosts_list.txt | ./pipe.sh ping -c1 | grep \"from\""
         echo
         exit 1
